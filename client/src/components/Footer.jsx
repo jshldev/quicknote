19f3acb3943f0845e59fad848149ff1988d6c6aa
jshldev <shleung.dev@gmail.com>
2025-05-02 20:07:02 +0800

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <footer>&copy; CopyRight {new Date().getFullYear()} Leung Sze Ho</footer>
-  );
+  return <footer>{new Date().getFullYear()} &copy; Leung Sze Ho</footer>;
 }
 
 export default Footer;
